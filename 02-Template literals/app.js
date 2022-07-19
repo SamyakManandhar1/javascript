164f8685d1c1f1097370772193a98fd5232c379b
samyak <samyakmanandhar2000@gmail.com>
2022-07-19 14:29:33 +0545

    template literals

--- 02-Template literals/app.js
+++ 02-Template literals/app.js
@@ -1,1 +1,21 @@
-console.log("ram");
+const name = "samyak";
+const age = "31";
+const job = "Freelancer";
+const city = "NewYoek";
+let html;
+
+function hello(){
+  return'hi Rakshita';
+}
+html = `
+    <ul>
+    <li>Name: ${name}</li>
+    <li>Age: ${age}</li>
+    <li>Job: ${job}</li>
+    <li>City: ${city}</li>
+    <li> ${2 + 2}</li>
+    <li> ${hello()}</li>
+    <li> ${age>30? 'over 30' : 'below 30'}</li>
+    </ul>
+`;
+document.body.innerHTML = html;
