bbe432e963871276e123e27f401ef56dbf1bba39
samyak <samyakmanandhar2000@gmail.com>
2022-07-19 13:32:55 +0545

    Starting with basic js  fundamentals

---
+++ app.js
@@ -0,0 +1,76 @@
+//Type Conversion
+
+let num;
+
+//To String
+
+num =String(55);
+
+//Boolean to String 
+
+num = String(true);
+
+//Date to String
+
+num = String(new Date())
+
+//Array to String
+
+num = ([1,2,3,4]);
+num = (2).toString();
+num =false.toString();
+num = Math.round(2.4);
+
+//Print
+
+//console.log(num);
+// console.log(typeof num);
+// console.log(num.length);
+
+const fname = 'Samyak';
+const lname = 'Manandhar';
+const age = '21';
+
+let val;
+
+    // val = fname + lname;
+
+//cocatenation
+    // val = fname + ' ' + lname;
+
+// append
+    // val = 'Sam';
+    // val += 'yak';
+    // val = "My name is " + fname + ' ' + lname + ",and i am " + age + " years old. :)"
+
+// escaping
+    // val = 'that\'s it!'; 
+
+// length 
+    // val = fname.length; 
+
+//concat 
+    // val = fname.concat(" sexy ", lname);
+
+//Change case
+    // val = fname.toUpperCase();
+    // val = lname.toLowerCase();
+
+    // val = fname[1];
+    // val = fname.indexOf('m');
+    // val = fname.charAt('3');
+
+//Substring
+    // val = fname.substring(0,4);
+
+//Slice
+    // val = fname.slice(0,3);
+    // val = fname.slice(-3);
+
+const str = "hello, My name is SamYak";
+
+    // val = str.split(",");
+    // val = str.replace("SamYak", "SAMYAK");
+    // val = str.includes("hello");
+    // val = str.includes("hi");
+console.log(val);
